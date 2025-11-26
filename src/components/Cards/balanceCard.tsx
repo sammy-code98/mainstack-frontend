55528d1f969a17ffb40c7d3b7c495ee0e3463a99
sammy-code98 <sammysmart98@yahoo.com>
2025-11-27 00:44:23 +0100

    fix: adjust balace card component

--- src/components/Cards/balanceCard.tsx
+++ src/components/Cards/balanceCard.tsx
@@ -7,14 +7,14 @@ interface BalanceCardProps {
 
 export default function BalanceCard({ title, amount }: BalanceCardProps) {
   return (
-    <div className="rounded-sm p-4 mb-4 cursor-pointer">
+    <div className="rounded-sm p-2 mb-2.5 cursor-pointer">
       <div className="flex justify-between items-center">
         <p className="text-lg text-mainstack-gray font-medium">{title}</p>
         <div className="text-mainstack-gray">
           <MdInfoOutline size="1.5rem" />
         </div>
       </div>
-      <h3 className="text-mainstack-primary text-2xl lg:text-4xl font-bold mt-2">
+      <h3 className="text-mainstack-primary text-2xl lg:text-3xl font-bold mt-2">
         USD {amount}
       </h3>
     </div>
