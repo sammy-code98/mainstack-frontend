69b028565b43f8b7c92c3d4a7f7180f61a5b7c46
sammy-code98 <sammysmart98@yahoo.com>
2025-11-26 01:02:39 +0100

    clean up

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-    <App />
+      <App />
     </QueryClientProvider>
   </StrictMode>,
 );
